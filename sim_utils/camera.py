3c6e1f7d84fe484dc90945ced0be15ad41c6ebe6
Barbany <oriolbm96@gmail.com>
2022-05-04 14:23:46 +0200

    Intrinsics from Euler + General rotation + H/V FOV

--- sim_utils/camera.py
+++ sim_utils/camera.py
@@ -1,8 +1,9 @@
 import numpy as np
 
 
-def get_camera_extrinsic_matrix(azimuth, distance, elevation, lookat):
+def get_camera_extrinsic_matrix_lookat(azimuth, distance, elevation, lookat):
     """Get the camera extrinsic matrix converting world coordinates to the camera referential.
+    Compute the parameters using a look at camera.
 
     Args:
         azimuth (float): Azimuth angle in radians.
@@ -15,64 +16,98 @@ def get_camera_extrinsic_matrix(azimuth, distance, elevation, lookat):
         np.ndarray: 3x4 extrinsic matrix.
     """
     rotation_matrix = (
-        rotate_z(np.pi) @ rotate_x(elevation + np.pi / 2) @ rotate_z(azimuth)
+        get_rotation_matrix(np.pi, [0, 0, 1])
+        @ get_rotation_matrix(elevation + np.pi / 2, [1, 0, 0])
+        @ get_rotation_matrix(azimuth, [0, 0, 1])
     )
     translation_vector = np.array([0, 0, distance]) - rotation_matrix @ lookat
     return np.c_[rotation_matrix, translation_vector]
 
 
-def get_camera_intrinsic_matrix(width, height, vertical_fov):
+def get_camera_extrinsic_matrix_euler(cam_pos, cam_angles):
+    """Get the camera extrinsic matrix converting world coordinates to the camera referential.
+    Compute the parameters using the euler angles and camera position.
+
+    Args:
+        cam_pos (np.ndarray): Position of the camera in terms of the world coordinates.
+            NumPy array of shape 3 containing [x, y, z].
+        cam_angles (np.ndarray): Angle between each axis in the camera frame and the world frame.
+            NumPy array of shape 3 containing [cam_x_angle, cam_y_angle, cam_z_angle].
+
+    Returns:
+        np.ndarray: Camera extrinsics matrix
+    """
+    cam_x_angle, cam_y_angle, _ = cam_angles
+    matrix1 = get_rotation_matrix(-cam_x_angle, [0, 1, 0])
+    matrix2 = get_rotation_matrix(-cam_y_angle - np.pi, [1, 0, 0])
+    rotation_matrix = matrix2 @ matrix1
+    translation_matrix = np.eye(4)
+    translation_matrix[0:3, 3] = -cam_pos
+    return rotation_matrix @ translation_matrix
+
+
+def get_camera_intrinsic_matrix(width, height, horizontal_fov=None, vertical_fov=None):
     """Get the camera intrinsic matrix converting camera coordinates to image coordinates.
+    If one of the fields of view is not specified, it will be computed using the image dimensions.
 
     Args:
         width (int): Width of the rendered image.
         height (int): Height of the rendered image.
-        vertical_fov (float): Vertical field of view in radians.
+        horizontal_fov (float, optional): Horizontal field of view in radians.
+        vertical_fov (float, optional): Vertical field of view in radians.
 
     Returns:
         np.ndarray: 3x3 intrinsic matrix.
+
+    Raises:
+        AttributeError: If any of the field of views are specified.
     """
+    if horizontal_fov is None:
+        if vertical_fov is None:
+            raise AttributeError("You must specify the FOV in one or the two axes")
+        else:
+
+            horizontal_fov = 2.0 * np.arctan(np.tan(vertical_fov / 2) * height / width)
+    else:
+        if vertical_fov is None:
+            vertical_fov = 2.0 * np.arctan(np.tan(horizontal_fov / 2) * width / height)
+
     px, py = (width / 2, height / 2)
-
     fy = height / (2.0 * np.tan(vertical_fov / 2.0))
-
-    horizontal_fov = 2.0 * np.arctan(np.tan(vertical_fov / 2) * width / height)
     fx = width / (2.0 * np.tan(horizontal_fov / 2.0))
-
     return np.array([[fx, 0, px], [0, fy, py], [0, 0, 1.0]])
 
 
-def rotate_x(theta):
-    """Get the rotation matrix encoding a rotation along the x-axis.
+def get_rotation_matrix(angle, axis):
+    """Get the rotation matrix encoding a rotation along an arbitrary axis.
 
     Args:
-        theta (float): Angle of rotation in radians.
+        angle (float): Angle of rotation in radians.
+        axis (np.typing.ArrayLike): Axis of rotation (possibly unnormalized).
 
     Returns:
         np.ndarray: 3x3 rotation matrix.
     """
+    axis = axis / np.linalg.norm(axis)
+    s = np.sin(angle)
+    c = np.cos(angle)
+
     return np.array(
         [
-            [1, 0, 0],
-            [0, np.cos(theta), -np.sin(theta)],
-            [0, np.sin(theta), np.cos(theta)],
-        ]
-    )
-
-
-def rotate_z(theta):
-    """Get the rotation matrix encoding a rotation along the z-axis.
-
-    Args:
-        theta (float): Angle of rotation in radians.
-
-    Returns:
-        np.ndarray: 3x3 rotation matrix.
-    """
-    return np.array(
-        [
-            [np.cos(theta), -np.sin(theta), 0],
-            [np.sin(theta), np.cos(theta), 0],
-            [0, 0, 1],
+            [
+                axis[0] * axis[0] + (1.0 - axis[0] * axis[0]) * c,
+                axis[0] * axis[1] * (1.0 - c) - axis[2] * s,
+                axis[0] * axis[2] * (1.0 - c) + axis[1] * s,
+            ],
+            [
+                axis[0] * axis[1] * (1.0 - c) + axis[2] * s,
+                axis[1] * axis[1] + (1.0 - axis[1] * axis[1]) * c,
+                axis[1] * axis[2] * (1.0 - c) - axis[0] * s,
+            ],
+            [
+                axis[0] * axis[2] * (1.0 - c) - axis[1] * s,
+                axis[1] * axis[2] * (1.0 - c) + axis[0] * s,
+                axis[2] * axis[2] + (1.0 - axis[2] * axis[2]) * c,
+            ],
         ]
     )
